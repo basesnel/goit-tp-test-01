14ef1d1db914541dcef1d42f3cbad5596df09ffe
basesnel <111281846+basesnel@users.noreply.github.com>
2023-05-09 19:33:47 +0000

    Deploying to gh-pages from @ basesnel/goit-tp-test-01@b0d5c190d7d052a23940baaa75d61c9b54a53527 🚀

---
+++ static/js/415.44596bb3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_tp_test_01=self.webpackChunkgoit_tp_test_01||[]).push([[415],{415:function(e,t,s){s.r(t);var c=s(364),r=s(184);t.default=function(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)(c.Z,{text:"Welcome"}),(0,r.jsx)("p",{className:"App__welcome",children:'It is a test proect for user\'s tweet cards, created by React. You can evaluate the work of the project by clicking on the menu "Tweets".'})]})}}}]);
+//# sourceMappingURL=415.44596bb3.chunk.js.map
